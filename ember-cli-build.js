ad0f8c3aa4bbfaf83841c0e6e1dfae5fb4e71bd7
Alexey Syantovich <syantovich.a.s@gmail.com>
2022-10-26 10:54:37 +0300

    _redirects

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    babel: {
+      // enable "loose" mode
+      loose: true,
+      // don't transpile generator functions
+    },
     sassOption: {
       extention: 'scss',
     },
